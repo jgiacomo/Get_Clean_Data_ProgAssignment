5d402ca49e2a1a747e3108206804886c79e694b8
Jason Giacomo <jgiacomo@gmail.com>
2014-10-26 14:44:25 -0700

    Working code and descriptive files for submission to course.

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,42 @@
-# use dplyr to provide useful tools for working with this data set
+# This script works on the Human Activity Recognition Using Smartphones
+# dataset.[1] The script was produced as part of Programming Assignment for the
+# Getting and Cleaning Data course offered at Coursera.
+# 
+# Purpose: To extract the data from the relevent text files, merge the 'test'
+# and 'train' datasets, clean and tidy the resulting dataset and finally to
+# create a separate data table containing summarized data.
+# 
+# Author: Jason Giacomo
+# 
+# [1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. 
+# Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass 
+# Hardware-Friendly Support Vector Machine. International Workshop of Ambient 
+# Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+# 
+# The dataset used here was obtained from the following link from the Coursera
+# course webpage:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# The zip file was extracted and this script is meant to be run from the same
+# directory which contains the extracted 'UCI HAR Dataset' directory.
+# 
+# 
+# This script will make use of the dplyr package to provide useful tools for
+# working with this data set.
 library(dplyr)
 
-# read in the feature and activity labels, convert to dplyr tables, remove
-# unneaded data frames
-act_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+# Read in the feature and activity labels, convert to dplyr tables, remove the
+# data frames. This script must be run from the same directory that the 'UCI HAR
+# Dataset' directory is in.
+actLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
-act_labels <- tbl_df(act_labels)
+actLabels <- tbl_df(actLabels)
 features <- tbl_df(features)
 
 # give them logical column names
-names(act_labels) <- c("activityID", "activity")
+names(actLabels) <- c("activityID", "activity")
 names(features) <- c("featureID", "feature")
 
-# read in the yest data and convert to dplyr tables
+# read in the y_test, X_test, and subject_test data and convert to dplyr tables
 yTest <- read.table("UCI HAR Dataset/test/y_test.txt")
 xTest <- read.table("UCI HAR Dataset/test/X_test.txt")
 subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
@@ -20,29 +44,31 @@ yTest <- tbl_df(yTest)
 xTest <- tbl_df(xTest)
 subjectTest <- tbl_df(subjectTest)
 
-# we are going to make the feature names the column names for xTest. Before
-# we do that let's clean up the names by removing characters which are
-# invalid for column names and any duplicated text (i.e. "BodyBody")
+# The features from the feature table are the descriptors for the data colomns
+# in xTest. They will be used as column names for the xTest table but before
+# that can be done they should be cleaned up to remove characters which are
+# invalid in data frame column names (only alpha numeric, '.', and '_' are
+# valid) and any duplicated text (i.e. "BodyBody"). After inspecting the values
+# in features the following regular expression replacements should clean them up
+# while still maintaining obvious similarities to the original dataset.
 features$feature <- gsub("\\(|\\)|-|,", "_", features$feature)
 features$feature <- gsub("__|___", "_", features$feature)
 features$feature <- gsub("_$", "", features$feature)
 features$feature <- gsub("BodyBody", "Body", features$feature)
 
-# give each new data frame logical column names
-# using data.tables instead of data.frames might be better so I can use key
-# replacement later when translating IDs into actual activities.
+# give each new dplyr table logical column names
 names(yTest) <- "activityID"
 names(xTest) <- features$feature
 names(subjectTest) <- "subjectID"
 
-# join the activityID in yTest with the activities from act_labels
-yTest <- left_join(yTest, act_labels)
+# Join the activityID in yTest with the activities from actLabels so the
+# resulting dataset will have more readable activity labels instead of numbers.
+yTest <- left_join(yTest, actLabels)
 
-# bind the columns of subjectID from subjectTest, activity from yTest, and all
-# columns from xTest into a new table called testData
+# Bind the columns of subjectID from subjectTest, activity from yTest, and all
+# columns from xTest into a new table called testData.
 testData <- cbind(subjectTest, "activity" = yTest$activity, xTest)
-# convert into a dplyr table
-testData <- tbl_df(testData)
+testData <- tbl_df(testData)  # convert into a dplyr table
 
 # remove tables which are no longer needed
 rm("yTest", "xTest", "subjectTest")
@@ -59,40 +85,47 @@ names(yTrain)<- "activityID"
 names(xTrain) <- features$feature
 names(subjectTrain) <- "subjectID"
 
-yTrain <- left_join(yTrain, act_labels)
+yTrain <- left_join(yTrain, actLabels)
 
 trainData <- cbind(subjectTrain, "activity" = yTrain$activity, xTrain)
 trainData <- tbl_df(trainData)
 
 rm("yTrain", "xTrain", "subjectTrain")
 
-# now we can also remove act_labels and features
-rm("act_labels", "features")
+# now we can also remove actLabels and features
+rm("actLabels", "features")
 
 # merge the two tables into one
 activityData <- rbind(testData, trainData)
-activityData <- tbl_df(activityData)
+activityData <- tbl_df(activityData)  # make into dplyr table
 
 # remove testData and trainData
 rm("testData", "trainData")
 
-# A check for duplicate column names indicates there are so we need to address
-# this by inspecting the duplicate names.
-names(activityData)[duplicated(names(activityData))]
-
-# none of these columns uncludes mean or standard deviation data which is asked
-# for in part 2. So, we can safely remove these columns without any ill effect.
+# A check for duplicate column names indicates there are, so this needs to be
+# addressed before we can clean the data further. After inspecting the duplicate
+# column names, none of these columns uncludes mean or standard deviation data,
+# which is all we are going to keep. So, these columns can be removed so they
+# won't cause any problems while manipulating the data.
 activityData <- activityData[,-which(duplicated(names(activityData)))]
-ncol(activityData)  # = 477
 
-# now we can reduce the columns of our dataset down to just the ones which
-# contain means and standard deviations. This will be accomplished by using a
-# regular expresion with dplyr's select:matches method.
-mean_std_data <- select(activityData, matches("subjectID|activity|mean|std"))
+# Now the dataset can be reduced to just the subjectID, activity, and any 
+# columns containing mean or standard deviation (Std) data. This will be
+# accomplished by using a regular expresion with dplyr's select:matches method.
+activityDataMeansAndStds <- select(activityData, matches("subjectID|activity|mean|std"))
+rm(activityData)  # this table can now be removed
 
-# Now it's time to summarize our data. This will create a new data table which
-# calculates the mean for each activity and subject.
+# The data will be summarized by creating a new dplyr table which
+# calculates the mean for each column grouped by activity and subject.
 
-averageByActivityAndSubject <- mean_std_data %>%
+averageByActivityAndSubject <- activityDataMeansAndStds %>%
     group_by(activity, subjectID) %>%
     summarise_each(funs(mean))
+
+# write tables to txt files
+write.table(activityDataMeansAndStds, "activityDataMeansAndStds.txt",
+            row.names = FALSE)
+write.table(averageByActivityAndSubject, "averageByActivityAndSubject.txt",
+            row.names = FALSE)
+
+# ----------End Script----------
